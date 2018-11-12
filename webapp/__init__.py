53f02fea398a7deba64961563e73bf6d5bb5cd66
Bryan <lop00003@byui.edu>
2018-11-11 22:05:25 -0800

    Working login page. Verifies username and password. Register page is inserting data, but not redirecting yet.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, flash, request, redirect, url_for, session
+from flask import Flask, render_template, flash, request, redirect, url_for, session, escape
 from content_management import Content
 from dbconnect import connection
 from wtforms import Form, BooleanField, TextField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from MySQLdb import escape_string as thwart
 import gc
+import os
 
 
 TOPIC_DICT = Content()
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 @app.route('/main/')
@@ -36,31 +38,40 @@ def page_not_found(e):
 # 	except Exception as e:
 # 		return render_template("500.html", error= str(e))
 
-@app.route('/login/', methods=["GET","POST"])
+
+@app.route('/login/', methods = ["GET", "POST"])
 def login_page():
+	try:
+		error = ''
 
-    error = ''
-    try:
-	
-        if request.method == "POST":
-		
-            attempted_username = request.form['username']
-            attempted_password = request.form['password']
+		c, conn = connection()
+		if request.method == 'POST':
+			username = thwart(request.form['username']).decode()
+			data = c.execute('SELECT * FROM users WHERE username = ("%s");' % username)
+			data = c.fetchone()
 
-            #flash(attempted_username)
-            #flash(attempted_password)
 
-            if attempted_username == "admin" and attempted_password == "password":
-                return redirect(url_for('home'))
-				
-            else:
-                error = "Invalid credentials. Try Again."
+			if sha256_crypt.verify(request.form['password'], data[2]) and (data[1] == username):
+				session['logged_in'] = True
+				session['username'] = username
+				flash('You are now logged in')
+				return redirect(url_for('home'))
+			else:
+				error = 'Invalid credentials, try again'
+		gc.collect()
+		return render_template('login.html', error = error)
+	except Exception as e:
+		error = 'Invalid credentials, try again'
+		return render_template('login.html', error = error)
+#To DO (change password?)
 
-        return render_template("login.html", error = error)
-
-    except Exception as e:
-        #flash(e)
-        return render_template("login.html", error = error)
+# @app.route('/logout/')
+# @login_required
+# def logout_page():
+# 	if session['logged_in']:
+# 		session['logged_in'] = False
+# 		session['username'] = None
+# 	return redirect(url_for('main'))
 
 class RegistrationForm(Form):
     username = TextField('Username', [validators.Length(min=4, max=20)])
@@ -80,11 +91,11 @@ def register_page():
         if request.method == "POST" and form.validate():
             username = form.username.data
             email = form.email.data
-            password = sha256_crypt.encrypt((str(form.password.data)))
+            password = sha256_crypt.hash((str(form.password.data)))
             c, conn = connection()
 
             x = c.execute("SELECT * FROM users WHERE username = (%s)",
-                            (thwart(username)))
+                            (thwart(username),))
 
             if int(x) > 0:
 
@@ -102,50 +113,14 @@ def register_page():
                 session['logged_in'] = True
                 session['username'] = username
 
-                return redirect(url_for('home'))
+                return redirect(url_for("home"))
 
         return render_template("register.html", form = form)
     except Exception as e:
         return(str(e))
 
-
-# @app.route('/register/', methods=["GET","POST"])
-# def register_page():
-#     try:
-#             form = RegistrationForm(request.form)
-#             if request.method == "POST" and form.validate():
-#                 username = form.username.data
-#                 email = form.email.data
-#                 password = sha256_crypt.encrypt((str(form.password.data)))
-#                 c, conn = connection()
-
-#                 x = c.execute("SELECT * FROM users WHERE username = (%s)", 
-#                             (thwart(username)))
-
-#                 if int(x) > 0:
-#                     # flash("That username is already taken. Please use another")
-#                     return render_template('register.html', form = form)
-#                 else:
-#                     c.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)",
-#                         (thwart(username), thwart(password), thwart(email)))
-
-#                 conn.commit()
-
-#                 # flash("Thanks for registering!")
-#                 c.close()
-#                 conn.close()
-#                 gc.collect()
-
-#                 session['logged_in'] = True
-#                 session['username'] = username
-
-#                 return redirect(url_for('home'))
-#             return render_template("register.html", form = form)
-#     except Exception as e:
-#             return(str(e))
-
-
 if __name__ == "__main__":
-    app.run(debug = True)
+	# app.secret_key = os.urandom(24)
+	app.run()
 
 #home is dashboard
